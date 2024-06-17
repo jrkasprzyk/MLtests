07b025e153836446d40ee6951a2fb658f666f201
Joseph Kasprzyk <joseph.kasprzyk@colorado.edu>
2024-06-17 15:04:01 -0600

    Separated some functions into utils.py; continued updating workflow to tensorflow 2. Tried to implement error labeling but not finished yet -- different tensorflow data objects are causing headaches

--- flow_detection/cnn.py
+++ flow_detection/cnn.py
@@ -2,11 +2,15 @@ import tensorflow as tf
 import argparse
 from typing import Dict
 from pathlib import Path, PureWindowsPath, PurePosixPath
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+import yaml
+import numpy as np
 
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+
+from src.utils import set_output_path, set_supervisor_path
 
 # References
 # https://www.tensorflow.org/tutorials/images/classification
@@ -16,10 +20,9 @@ from tensorflow.keras.models import Sequential
 # https://campus.datacamp.com/courses/introduction-to-deep-learning-with-keras/improving-your-model-performance
 # https://github.com/jeffheaton/t81_558_deep_learning/blob/b59a6ca8334246a7b5358c81b9c7b1849bb45371/t81_558_class_06_2_cnn.ipynb
 
-# TODO verify validation vs test
-# TODO try larger image size
-# TODO setup labeling of true and false classification results: see Jun's code
-# TODO finish setting up arg parser
+# TODO check Matplotlib in conda environment (error on seecdesktop); debug "import Sequential" error
+# TODO continue learning about batch prediction and error labeling
+# TODO implement cross validation
 
 #################
 # User Parameters
@@ -31,64 +34,16 @@ from tensorflow.keras.models import Sequential
 # Path to the output file
 #output_file = r"C:\Users\josep\Documents\GitHub\MLtests\flow_detection\model_target680_batch16.keras"
 
-GLOBAL_SETTINGS = {
-    'batch_size': 24,
-    'edge_size': 128,
-    'epochs': 15,
-    'learning_rate': 1e-3,
-    'validation_split': 0.2,
-    'trial_label': 'edge128batch24'
-}
-
-def get_args() -> Dict:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--computer',
-        choices=['hpc', '2020laptop', 'seec_desktop'],
-        type=str,
-        help="Computer - dictates paths to supervisor data"
-    )
-    parser.add_argument(
-        '--batch_size',
-        type=int,
-        required=False,
-        help="Batch size"
-    )
-    parser.add_argument(
-        '--edge_size',
-        type=int,
-        help="Edge size - for image resizing"
-    )
-    cfg = vars(parser.parse_args())
-
-    # combine global settings with user config
-    cfg.update(GLOBAL_SETTINGS)
-
-    # insert validation checks here
-
-    # add GPU device call here?
-
-    return cfg
-
-def set_supervisor_path(computer) -> Path:
-    if computer == 'hpc':
-        return Path("/projects/joka0958/supervisor/")
-    elif computer == '2020laptop':
-        return Path("C:/Users/josep/OneDrive - UCB-O365/Students/_shares/Lee HUB/junresearch/DeeplearningCNN_flow_detection/supervisor")
-
-def set_output_path(computer) -> Path:
-    if computer == 'hpc':
-        return Path("/projects/joka0958/MLoutput/")
-    elif computer == '2020laptop':
-        return Path("C:/Users/josep/Documents/GitHub/MLtests/flow_detection/")
-
-
 #################
 # Model creation and testing
 ################
 
 def main() -> None:
-    config = get_args()
+
+    config = yaml.safe_load(open("config.yaml"))
+
+    # show config
+    print(config)
 
     supervisor_path = set_supervisor_path(config["computer"])
     output_path = set_output_path(config["computer"])
@@ -101,41 +56,46 @@ def main() -> None:
     # splitting this into two generators so that the training data are augmented
     # but the validation data are not
 
-    train_ds = tf.keras.utils.image_dataset_from_directory(
+    # image_dataset_from_directory works differently than flow_from_directory
+    # and this article suggests to skip it altogether and build your own
+    # data pipeline:
+    # https://medium.com/p/215e594f2435
+
+    train_batched_ds, val_batched_ds = tf.keras.utils.image_dataset_from_directory(
         supervisor_path,
         validation_split=config['validation_split'],
-        subset="training",
+        subset="both",
         seed=123,
         image_size=(config["edge_size"],
                     config["edge_size"]),
         batch_size=config["batch_size"]
     )
 
-    val_ds = tf.keras.utils.image_dataset_from_directory(
+    # only the validation data, no batches
+    val_single_ds = tf.keras.utils.image_dataset_from_directory(
         supervisor_path,
         validation_split=config['validation_split'],
         subset="validation",
         seed=123,
         image_size=(config["edge_size"],
                     config["edge_size"]),
-        batch_size=config["batch_size"]
+        batch_size=None
     )
 
-    class_names = train_ds.class_names
+    class_names = val_batched_ds.class_names
     num_classes = len(class_names)
 
     model = Sequential([
-        layers.Rescaling(1. / 255, input_shape=(config["edge_size"],
-                                                config["edge_size"],
-                                                3)),
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.Input(shape=(config["edge_size"], config["edge_size"], 3)),
+        layers.Rescaling(1. / 255),
+        layers.Conv2D(16, 3, padding='same', activation=config["activation_function"]),
         layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.Conv2D(32, 3, padding='same', activation=config["activation_function"]),
         layers.MaxPooling2D(),
-        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.Conv2D(64, 3, padding='same', activation=config["activation_function"]),
         layers.MaxPooling2D(),
         layers.Flatten(),
-        layers.Dense(128, activation='relu'),
+        layers.Dense(128, activation=config["activation_function"]),
         layers.Dense(num_classes)
     ])
 
@@ -146,51 +106,56 @@ def main() -> None:
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
 
-    model.summary()
+    if config["verbose"]:
+        model.summary()
 
     # model training
     history = model.fit(
-        train_ds,
+        train_batched_ds,
         epochs=config["epochs"],  # Set epochs count
-        validation_data=val_ds,
+        validation_data=val_batched_ds,
     )
 
-    # label errors
-    
+    # Save the model
+    if config["save_model"]:
+        model.save(output_path / (config["trial_label"] + ".keras"))
 
     # plot history
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
+    if config["plot_history"]:
+        acc = history.history['accuracy']
+        val_acc = history.history['val_accuracy']
 
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
 
-    epochs_range = range(config["epochs"])
+        epochs_range = range(config["epochs"])
 
-    plt.figure(figsize=(8, 8))
-    plt.subplot(1, 2, 1)
-    plt.plot(epochs_range, acc, label='Training Accuracy')
-    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-    plt.legend(loc='lower right')
-    plt.title('Training and Validation Accuracy')
+        plt.figure(figsize=(8, 8))
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs_range, acc, label='Training Accuracy')
+        plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+        plt.legend(loc='lower right')
+        plt.title('Training and Validation Accuracy')
 
-    plt.subplot(1, 2, 2)
-    plt.plot(epochs_range, loss, label='Training Loss')
-    plt.plot(epochs_range, val_loss, label='Validation Loss')
-    plt.legend(loc='upper right')
-    plt.title('Training and Validation Loss')
-    plt.show()
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, loss, label='Training Loss')
+        plt.plot(epochs_range, val_loss, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+        plt.show()
 
-    # Model evaluation and self-diagnosis
-    #test_loss, test_accuracy = model.evaluate(val_data)
-    #print(f'Test accuracy: {test_accuracy}')
+    # model.evaluate...including batching
 
-    # Image classification prediction
-    #predictions = model.predict(val_data)
+    if config["verbose"]:
+        print("WITH VALIDATION BATCHING")
+        print("Evaluating model behavior...")
 
-    # Specify the path and file name to save the model C:/path/file_name.h5
-    model.save(output_path / (config["trial_label"] + ".keras"))
+    val_loss, val_acc = model.evaluate(val_batched_ds)
 
+    if config["verbose"]:
+        print("loss=", val_loss, " acc=", val_acc)
+
+    # see individual image prediction in cnn_from_file
 
 if __name__ == '__main__':
     main()
--- flow_detection/cnn_from_file.py
+++ flow_detection/cnn_from_file.py
@@ -2,52 +2,31 @@ import tensorflow as tf
 from pathlib import Path
 import numpy as np
 
-def set_supervisor_path(computer) -> Path:
-    if computer == 'hpc':
-        return Path("/projects/joka0958/supervisor/")
-    elif computer == '2020laptop':
-        return Path("C:/Users/josep/OneDrive - UCB-O365/Students/_shares/Lee HUB/junresearch/DeeplearningCNN_flow_detection/supervisor")
-
-def set_output_path(computer) -> Path:
-    if computer == 'hpc':
-        return Path("/projects/joka0958/MLoutput/")
-    elif computer == '2020laptop':
-        return Path("C:/Users/josep/Documents/GitHub/MLtests/flow_detection/")
+from src.utils import set_output_path, set_supervisor_path
 
 def main() -> None:
-    supervisor_path = set_supervisor_path("2020laptop")
-    output_path = set_output_path("2020laptop")
+    supervisor_path = set_supervisor_path("seecdesktop")
+    output_path = set_output_path("seecdesktop")
 
-    my_filename = output_path / "edge64test.keras"
+    my_filename = output_path / "edge256batch128.keras"
 
     # load model from file
     # https://www.tensorflow.org/tutorials/keras/save_and_load
     model = tf.keras.models.load_model(my_filename)
 
-    # TODO what is difference between model.evaluate and model.predict?
-
     # set up validation data
     # note: will these parameters have to be the same as
     # the initial run? should they be saved so they can
     # be pulled in from a file?
-    val_ds = tf.keras.utils.image_dataset_from_directory(
+    val_single_ds = tf.keras.utils.image_dataset_from_directory(
         supervisor_path,
         validation_split=0.2,
         subset="validation",
         seed=123,
-        image_size=(64,
-                    64),
-        batch_size=64
+        image_size=(256,
+                    256),
+        batch_size=128
     )
-
-    print("beginning evaluation")
-    my_loss, my_acc = model.evaluate(val_ds)
-    print("loss=", my_loss, " acc=", my_acc)
-
-    print("beginning prediction")
-    predictions = model.predict(val_ds, verbose=2)
-    predicted_classes = np.argmax(predictions, axis=1)
-    print("predictions complete")
 
     # based on:
     # another tutorial that shows off different functions
@@ -57,7 +36,16 @@ def main() -> None:
     # which discusses things like rebatch and other actions on a Data object...
 
     print("pull one image")
-    x, y = next(iter(val_ds.rebatch(1)))
+    x, y = next(iter(val_single_ds))
+    image = x[0, :, :, :]
+    true_index = np.argmax(y[0])
+    prediction_scores = model.predict(np.expand_dims(image, axis=0))
+    predicted_index = np.argmax(prediction_scores)
+    print("True class: ", true_index)
+    print("Predicted class: ", predicted_index)
+
+    print("and again")
+    x, y = next(iter(val_single_ds))
     image = x[0, :, :, :]
     true_index = np.argmax(y[0])
     prediction_scores = model.predict(np.expand_dims(image, axis=0))
@@ -67,13 +55,6 @@ def main() -> None:
 
     pass
 
-    # in old version, this was:
-    #true_classes = val_ds.classes
-    #true_class_labels = val_ds.class_indices.keys()
-
-    #true_classes = val_ds.class_func
-    #print(true_classes)
-
     # this seems to discuss how to manipulate data in tensorflow 2.16.1
     # https://www.tensorflow.org/tutorials/load_data/images
     # problem is: I don't understand how to get the basic data extracted from val_ds
@@ -81,6 +62,10 @@ def main() -> None:
 
     # see here for objective functions:
     # https://github.com/jeffheaton/t81_558_deep_learning/blob/master/t81_558_class_04_2_multi_class.ipynb
+
+    # challenge: seems like there is still no straightforward way to pull the true class
+    # for each datapoint when the data are loaded in like this
+    # also: do the predictions always have to be done in batches?
 
 if __name__ == "__main__":
     main()
