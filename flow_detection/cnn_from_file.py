0e26859793f91fce0d08ffae9de48b0326e83610
Joseph Kasprzyk <joseph.kasprzyk@colorado.edu>
2024-06-17 10:38:15 -0600

    Continued to learn about evaluate and predict. "true_class" for whole dataset not working yet

--- flow_detection/cnn_from_file.py
+++ flow_detection/cnn_from_file.py
@@ -18,7 +18,11 @@ def main() -> None:
     supervisor_path = set_supervisor_path("2020laptop")
     output_path = set_output_path("2020laptop")
 
+    my_filename = output_path / "edge64test.keras"
+
     # load model from file
+    # https://www.tensorflow.org/tutorials/keras/save_and_load
+    model = tf.keras.models.load_model(my_filename)
 
     # TODO what is difference between model.evaluate and model.predict?
 
@@ -31,13 +35,52 @@ def main() -> None:
         validation_split=0.2,
         subset="validation",
         seed=123,
-        image_size=(128,
-                    128),
+        image_size=(64,
+                    64),
         batch_size=64
     )
 
-    predictions = model.predict(val_ds)
-    predicted_classes = np.argmax(predictions, axis=1)
+    print("beginning evaluation")
+    my_loss, my_acc = model.evaluate(val_ds)
+    print("loss=", my_loss, " acc=", my_acc)
 
-    true_classes = val_ds.classes
-    true_class_labels = val_ds.class_indices.keys()
+    print("beginning prediction")
+    predictions = model.predict(val_ds, verbose=2)
+    predicted_classes = np.argmax(predictions, axis=1)
+    print("predictions complete")
+
+    # based on:
+    # another tutorial that shows off different functions
+    # https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/hub/tutorials/tf2_image_retraining.ipynb#scrollTo=umB5tswsfTEQ
+    # and:
+    # https://www.tensorflow.org/api_docs/python/tf/data/Dataset
+    # which discusses things like rebatch and other actions on a Data object...
+
+    print("pull one image")
+    x, y = next(iter(val_ds.rebatch(1)))
+    image = x[0, :, :, :]
+    true_index = np.argmax(y[0])
+    prediction_scores = model.predict(np.expand_dims(image, axis=0))
+    predicted_index = np.argmax(prediction_scores)
+    print("True class: ", true_index)
+    print("Predicted class: ", predicted_index)
+
+    pass
+
+    # in old version, this was:
+    #true_classes = val_ds.classes
+    #true_class_labels = val_ds.class_indices.keys()
+
+    #true_classes = val_ds.class_func
+    #print(true_classes)
+
+    # this seems to discuss how to manipulate data in tensorflow 2.16.1
+    # https://www.tensorflow.org/tutorials/load_data/images
+    # problem is: I don't understand how to get the basic data extracted from val_ds
+    # in other words: what is the true class for each image? What is the predicted class for each image?
+
+    # see here for objective functions:
+    # https://github.com/jeffheaton/t81_558_deep_learning/blob/master/t81_558_class_04_2_multi_class.ipynb
+
+if __name__ == "__main__":
+    main()
